cc5071c6b24379beddb9837d6c9871065b083163
Tomas Karabela <tkarabela@seznam.cz>
2024-07-26 23:45:46 +0200

    fix: The multiprocessing module now serializes exceptions properly
    See issue #97.

--- tests/formats/test_formats.py
+++ tests/formats/test_formats.py
@@ -1,3 +1,5 @@
+import multiprocessing
+import itertools
 import pytest
 import pysubs2
 
@@ -24,3 +26,25 @@ def test_format_detection_fail() -> None:
     with pytest.raises(pysubs2.FormatAutodetectionError) as exc_info:
         pysubs2.formats.autodetect_format("")
     assert not exc_info.value.formats
+
+
+def _test_97_func(i: int, error: int) -> int:
+    if i == 0:
+        if error == 0:
+            raise RuntimeError("Regular exception is properly handled by multiprocessing")
+        elif error == 1:
+            pysubs2.SSAFile.from_string("")
+        else:
+            assert False
+    else:
+        return i
+
+
+@pytest.mark.timeout(5)
+def test_97_format_detection_multiprocessing_error() -> None:
+    # see issue #97
+    with multiprocessing.Pool(4) as pool:
+        with pytest.raises(RuntimeError):
+            pool.starmap(_test_97_func, zip(range(4), itertools.repeat(0)))
+        with pytest.raises(pysubs2.FormatAutodetectionError):
+            pool.starmap(_test_97_func, zip(range(4), itertools.repeat(1)))
